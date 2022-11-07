4405bdad4d903cdaa480623530954abc8cbdb132
alfredmm <alfredmutua2030@gmail.com>
2022-11-07 19:00:03 +0300

    Version 1

---
+++ area_of_triangle.py
@@ -0,0 +1,14 @@
+# A program that calculates the area of a triangle
+# From user input
+# 1/2 * B * H
+
+def area_of_triangle():
+    base = int(input("Enter Base of The Triangle: "))
+    height = int(input("Enter Height of The Triangle: "))
+    area = 1/2 * base * height
+
+    print("The Area=",area)
+
+area_of_triangle()
+    
+    
